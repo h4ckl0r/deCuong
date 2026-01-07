ddba6b2fb08fd9f3e93f46f977b801ff4ebe062d
dangtoan <dangtoan.2k6@gmail.com>
2026-01-07 20:50:28 +0700

    fix

--- src/components/Quizz.tsx
+++ src/components/Quizz.tsx
@@ -11,15 +11,13 @@ import {
 // --- CẤU HÌNH API VÀ TOKEN ---
 const TEST_ID: number = 177;
 
-// Lấy URL từ biến môi trường. Nếu không có thì mặc định là chuỗi rỗng
-const API_URL = "";
-
 // Ghép chuỗi để tạo thành đường dẫn đầy đủ
 // Kết quả sẽ là: http://localhost:8080/api-rikkei/tests/177
-const BASE_API_PATH: string = `${API_URL}/api-rikkei/tests/${TEST_ID}`;
+const BASE_API_PATH: string = `/api-rikkei/tests/${TEST_ID}`;
 
 // Lấy Token từ file .env
-const AUTH_TOKEN: string = import.meta.env.VITE_AUTH_TOKEN || "";
+const AUTH_TOKEN: string =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJpa2tlaXJkQGdtYWlsLmNvbSIsIm5hbWUiOiJSJkQiLCJpZCI6ODMsInJvbGUiOlt7ImlkIjoxLCJuYW1lIjoiQURNSU4ifSx7ImlkIjoyLCJuYW1lIjoiTUFOQUdFUiJ9LHsiaWQiOjMsIm5hbWUiOiJURUFDSEVSIn0seyJpZCI6NCwibmFtZSI6IlRFQUNIRVJfQVNTSVNUQU5UIn0seyJpZCI6NSwibmFtZSI6IkdVRVNUIn1dLCJ0eXBlIjoidXNlciIsImlhdCI6MTc2Nzc5Mzc2MSwiZXhwIjoxNzY3ODgwMTYxfQ.WLW9dsPP7O8DX-dbL8hl1aj1w-4U4i8x8wYAQ_AV_bM";
 // --- INTERFACES ---
 interface OptionTest {
   id: number;
