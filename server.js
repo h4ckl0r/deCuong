363487ce62a7658ada7da0a1841533b62bfb12dd
dangtoan <dangtoan.2k6@gmail.com>
2026-01-07 17:17:11 +0700

    Fix routing error using regex

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Tạo __dirname (vì trong ES Modules biến này không có sẵn)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -18,22 +17,21 @@ app.use(
     changeOrigin: true,
     pathRewrite: { "^/api-rikkei": "" },
     onProxyReq: (proxyReq) => {
-      // Fake headers để đánh lừa server Rikkei
       proxyReq.setHeader("Origin", "https://portal.rikkei.edu.vn");
       proxyReq.setHeader("Referer", "https://portal.rikkei.edu.vn/dangnhap");
     },
   })
 );
 
-// 2. Serve file tĩnh từ thư mục dist (sau khi build xong)
+// 2. Serve file tĩnh
 app.use(express.static(path.join(__dirname, "dist")));
 
-// 3. Xử lý React Router (để khi F5 ở trang con không bị lỗi 404)
-app.get("*", (req, res) => {
+// 3. Xử lý React Router (Sửa lỗi crash tại đây)
+// Dùng Regex /.*/ để khớp với tất cả các đường dẫn
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-// Khởi chạy server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
